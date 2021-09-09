21549bd1eda460985f432d3b69f534705e311a1b
moll-y <mol.tired@gmail.com>
2021-09-09 13:11:26 -0500

    add list item table

---
+++ src/app/items/item-list/item-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss'],
+})
+export class ItemListComponent implements OnInit {
+  @Input() items = [];
+
+  ngOnInit() {}
+}
