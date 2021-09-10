b5f744fe1ca6982e61c0024d8a4ec9c0ca61b37a
moll-y <mol.tired@gmail.com>
2021-09-10 17:47:05 -0500

    add delete item modal

--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 import { Item } from '@app/items/shared/item.model';
 import { ItemService } from '@app/items/shared/item.service';
 
@@ -10,25 +12,38 @@ import { ItemService } from '@app/items/shared/item.service';
 })
 export class ItemListComponent implements OnInit {
   @Input() items: Item[];
+  itemToDelete: Item;
 
   // pagination params
   readonly pageSize = 5;
   collectionSize = 0;
   page = 1;
 
-  constructor(private itemService: ItemService) {}
+  constructor(private modalService: NgbModal, private itemService: ItemService) {}
 
   ngOnInit() {
-    this.itemService.get().subscribe(
-      (pagination) => {
-        this.items = pagination.data;
-        this.collectionSize = pagination.total;
-      },
-      (err) => console.log(err)
-    );
+    this.fetchItemsPaginatedAt(this.page);
   }
 
   pageChange(page: number): void {
+    this.fetchItemsPaginatedAt(page);
+  }
+
+  open(content, index: number) {
+    this.itemToDelete = this.items[index];
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-title' }).result.then(
+      (result) => {
+        this.itemService.remove({ ...this.itemToDelete }).subscribe(
+          (_) => this.fetchItemsPaginatedAt(this.page),
+          (err) => console.log(err)
+        );
+      },
+      (reason) => {}
+    );
+  }
+
+  private fetchItemsPaginatedAt(page: number): void {
     this.itemService.get(page).subscribe(
       (pagination) => {
         this.items = pagination.data;
